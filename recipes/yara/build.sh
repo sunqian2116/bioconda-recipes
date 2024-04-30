189e29b2a80ac190795386d6c4b6b4a35b1be173
Akirarrr <luo_x1ang@163.com>
2024-04-30 11:38:33 +0800

    linux-aarch64 build for yara

--- recipes/yara/build.sh
+++ recipes/yara/build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -eu -o pipefail
 
+cd $SRC_DIR
+echo ${CXX}
+
+mkdir ../yara-build
+cd ../yara-build
+cmake ../work -DSEQAN_BUILD_SYSTEM=APP:yara -DCMAKE_CXX_COMPILER=${CXX}
+
+make all
+
 mkdir -p $PREFIX/bin
 
 binaries="\
@@ -8,8 +17,18 @@ yara_mapper \
 yara_indexer \
 "
 
+cd bin
+
 for i in $binaries
 do
-    cp $SRC_DIR/bin/$i $PREFIX/bin/$i
-    chmod a+x $PREFIX/bin/$i
+	cp $i $PREFIX/bin/$i
+	chmod a+x $PREFIX/bin/$i
 done
+
+
+#sed -i '/#include <zlib.h>/d' ../work/include/seqan/stream/stream_compressor.h
+#sed -i '/#include <zlib.h>/d' ../work/include/seqan/stream.h
+
+#echo "${PREFIX}"
+#mkdir $PREFIX/bin/
+#cp bin/* $PREFIX/bin/
